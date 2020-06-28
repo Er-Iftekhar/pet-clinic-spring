5b5f47eaa94549aa4eec801feb27aff157a79558
SyedIftekharuddin <syedy636@gmail.com>
2020-06-28 21:38:46 +0200

    Intial Project Add

---
+++ src/main/java/com/app/utb/petclinicspring/PetClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.app.utb.petclinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringApplication.class, args);
+    }
+
+}
