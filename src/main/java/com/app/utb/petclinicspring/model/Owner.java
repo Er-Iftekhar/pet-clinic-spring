052bfa8b99b18f3e69a8a812ba414b3fdb8f4f22
SyedIftekharuddin <syedy636@gmail.com>
2020-07-01 18:48:38 +0200

    Adding POJOs. Closes #2

---
+++ src/main/java/com/app/utb/petclinicspring/model/Owner.java
@@ -0,0 +1,4 @@
+package com.app.utb.petclinicspring.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/app/utb/petclinicspring/model/Vet.java
@@ -0,0 +1,6 @@
+package com.app.utb.petclinicspring.model;
+
+public class Vet extends Person {
+
+
+}
